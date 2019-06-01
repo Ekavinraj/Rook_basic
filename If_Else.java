8d04f0a231e63139edf48a09e21e5c2fc7cfeb4d
kavin <47354507+Ekavinraj@users.noreply.github.com>
2019-06-01 23:24:23 +0530

    Add files via upload

---
+++ If_Else.java
@@ -0,0 +1,23 @@
+package IfElse;
+
+import java.util.*;
+
+public class If_Else {
+
+	public static void main(String[] args) {
+
+		Scanner d = new Scanner(System.in);
+		System.out.print("Enter the age: ");
+		int age = d.nextInt();
+		if (age >= 18) {
+			System.out.println("Age is greater than 18,you are major");
+		} else if (age < 18) {
+			System.out.println("Age is lesser than 18 ,you are minor");
+
+		} else {
+			System.out.println("Invalid age");
+		}
+		d.close();
+
+	}
+}
